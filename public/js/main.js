1980a0b3b38866ec3deca37356ec96c23efaff06
mohamed <ramdam.Mohamed1080@hotmail.com>
2021-12-16 20:02:30 +0100

    js

--- public/js/main.js
+++ public/js/main.js
@@ -1,108 +1,48 @@
-// const counter = document.querySelector('h1')
-// let counterd = 0
-
-
-// const b = () => {
-
-
-//     const bubble = document.createElement('span')
-//     document.body.appendChild(bubble)
-//     const size = Math.random() * 200 + 100 + "px"
-
-//     bubble.classList.add('bubble')
-
-//     bubble.style.height = size;
-//     bubble.style.width = size;
-
-
-//     // const po = Math.random()*100+"%"
-
-//     bubble.style.top = Math.random() * 100 + 50 + "%";
-//     bubble.style.left = Math.random() * 100 + '%';
-
-
-//     const plusminus = Math.random() > 0.5 ? 1 : -1
-//     bubble.style.setProperty('--left', Math.random() * 100 * plusminus + '%')
-
-
-//     bubble.addEventListener('click', () => {
-//         counterd++
-//         counter.textContent = counterd
-
-
-//         bubble.remove()
-
-//     })
-
-
-//     setTimeout(() => {
-//         bubble.remove()
-
-//     }, 8000);
-
-
-
-// }
-
-
-
-
-// setInterval(() => {
-//     b()
-
-// }, 300);
-
-const h1 = document.querySelector('h1')
-const bouton = document.querySelector('button')
-
+const score = document.querySelector('h1')
+const btn = document.querySelector('button')
 let counter = 0
 
 
-const go = () =>{
+const bullle = () => {
     const bubble = document.createElement('span')
-document.body.appendChild(bubble)
+    document.body.appendChild(bubble)
 
-const size = Math.random()*200+100+'px'
+    const size = Math.random() * 200 + 80 + 'px'
 
-bubble.style.height= size;
-bubble.style.width= size;
-bubble.classList.add('bubble')
-
-bubble.style.top =  Math.random()*80+50+'%'
-bubble.style.left =  Math.random()*80+20+'%'
-
-const plusminus = Math.random() > 0.5 ? 1 :-1
+    bubble.style.height = size;
+    bubble.style.width = size;
+    bubble.classList.add('bubble')
+    bubble.style.top = Math.random() * 30 + 70 + '%'
+    bubble.style.left = Math.random() * 100 + '%'
 
 
-bubble.style.setProperty('--left',Math.random()*80*plusminus+'%')
-setTimeout(() => {
-    bubble.remove()
-}, 6000);
+    const plusminus = Math.random() > 0.5 ? 1 : -1
+    bubble.style.setProperty('--left', Math.random() * 80 * plusminus + '%')
 
+    setTimeout(() => {
+        bubble.remove()
+    }, 6000);
 
-bubble.addEventListener('click',()=>{
+    bubble.addEventListener('click', () => {
+
+        bubble.remove()
+        counter++
+        score.textContent = counter
+
+    })
+
+btn.addEventListener('click',()=>{
 
 bubble.remove()
-counter++
-h1.textContent=counter
+counter=0
+score.textContent=counter
 
 })
-bouton.addEventListener('click' ,()=>{
-
-bubble.remove()
-counter=0-1
-
-
-})
-
-
 
 }
 
 
+
 setInterval(() => {
-    go()
-    
+    bullle()
 }, 300);
-
-
